96e0b6897387fd5656877983144e1fe8d750c61f
yamiletarias1 <yamilet.arias2501@alumnos.ubiobio.cl>
2026-04-20 14:18:05 -0400

    clase Pasaporte arreglada, tambien creada la clase rut y la interface

--- src/Pasaporte.java
+++ src/Pasaporte.java
@@ -1,25 +1,40 @@
 
-public class Pasaporte  {
+public class Pasaporte {
 
-    private String Numero ;
-    private String nacionalidad ;
-    public Pasaporte (String num , String nacionalidad){
-        this.Numero = num ;
-        this.nacionalidad = nacionalidad ;
+    private String numero;
+    private String nacionalidad;
+
+    public Pasaporte(String num, String nacionalidad) {
+        this.numero = num;
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getNumero() {
+        return numero;
     }
 
     public String getNacionalidad() {
         return nacionalidad;
     }
 
-    public String getNumero(){
-        return Numero ;
-    }
 
-    public static  Pasaporte of (String num , String nacionalidad){
-        return new Pasaporte (num, nacionalidad);
+    public String toString() {
+        return numero + " " + nacionalidad;
     }
 
 
+    public boolean equals(Object obj) {
+        if (obj instanceof Pasaporte) {
+            Pasaporte p = (Pasaporte) obj;
+            return numero.equals(p.numero) && nacionalidad.equals(p.nacionalidad);
+        }
+        return false;
+    }
 
+    public static Pasaporte of(String num, String nacionalidad) {
+        if (num == null || nacionalidad == null) {
+            return null;
+        }
+        return new Pasaporte(num, nacionalidad);
+    }
 }
