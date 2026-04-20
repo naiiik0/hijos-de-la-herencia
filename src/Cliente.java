cb6ba0360cb055fb4876ea9ca5685d4461d0cf7c
naiiik0 <nicolas.figueroa2401@alumnos.ubiobio.cl>
2026-04-20 14:41:32 -0400

    Clase cliente hecha

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente extends Persona{
     private String email;
+    private List<Venta> ventas;
 
     public Cliente (IdPersona id, Nombre nom, String email){
-        this.id = id;
-        this.nom = nom;
+        super(id, nom);
         this.email = email;
+        this.ventas = new ArrayList<>();
     }
 
     public String getEmail() {
@@ -13,5 +17,11 @@ public class Cliente extends Persona{
 
     public void setEmail(String email) {
         this.email = email;
+    }
+    public void addVenta(Venta venta) {
+        ventas.add(venta);
+    }
+    public Venta[] getVentas() {
+        return ventas.toArray(new Venta[0]);
     }
 }
