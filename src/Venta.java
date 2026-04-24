8479d338bc397582985db0cbd2728646bf3d0545
yamiletarias1 <yamilet.arias2501@alumnos.ubiobio.cl>
2026-04-23 20:30:54 -0400

    clase venta creada

---
+++ src/Venta.java
@@ -0,0 +1,47 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Venta {
+
+    private String idDocumento;
+    private TipoDocumento tipo;
+    private LocalDate fecha;
+    private Cliente cliente;
+    private ArrayList<Pasaje> pasajes;
+
+    public Venta(String id, TipoDocumento tipo, LocalDate fecha, Cliente cliente) {
+        this.idDocumento = id;
+        this.tipo = tipo;
+        this.fecha = fecha;
+        this.cliente = cliente;
+        this.pasajes = new ArrayList<>();
+    }
+    public String getIdDocumento (){
+
+    }
+
+    public TipoDocumento getTipo() {
+        return tipo;
+    }
+    public LocalDate getFecha() {
+        return fecha;
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void createPasaje(int asiento, Viaje viaje, Pasajero pasajero) {
+        Pasaje p = new Pasaje(asiento, viaje, pasajero);
+        pasajes.add(p);
+    }
+    public Pasaje[] getPasajes() {
+        return pasajes.toArray(new Pasaje[0]);
+    }
+    public int getMonto() {
+        int total = 0;
+        for (Pasaje p : pasajes) {
+            total += p.getPrecio();
+        }
+        return total;
+    }
+    
+}
