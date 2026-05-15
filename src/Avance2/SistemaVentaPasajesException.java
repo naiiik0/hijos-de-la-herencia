30cb0f5a7415a1f2df187fa97e9c3f776a003f98
naiiik0 <nicolas.figueroa2401@alumnos.ubiobio.cl>
2026-05-15 16:09:38 -0400

    Clases finalizadas (Main, ControladorEmpresas y SistemaVentaPasajes)

--- src/Avance2/SistemaVentaPasajesException.java
+++
@@ -1,8 +0,0 @@
-package Avance2;
-// Autor: Juan Bustos
-public class SistemaVentaPasajesException extends RuntimeException {
-
-    public SistemaVentaPasajesException(String mensaje) {
-        super(mensaje);
-    }
-}
