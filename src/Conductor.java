794571978e79d7202590fc5c4a0072d91af6db63
sofi4488 <sofia.lagos2501@alumnos.ubiobio.cl>
2026-05-20 20:15:31 -0400

    conductor

---
+++ src/Conductor.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Conductor extends Tripulante{
+    private final List<Viaje> viajes = new ArrayList<>();
+
+    public Conductor(IdPersona id, Nombre nom, Direccion dir) {
+        super(id, nom, dir);
+    }
+
+    @Override
+    public void addViaje(Viaje viaje) {
+        viajes.add(viaje);
+
+    }
+
+    @Override
+    public int getNroViajes() {
+        return viajes.size();
+    }
+}
